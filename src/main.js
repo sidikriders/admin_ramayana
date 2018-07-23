34951c9eafd1e965fc2079f12da8ea9b6e902ed8
sidikriders <sidik@xwork.id>
2018-07-23 11:56:11 +0700

    add times font awesome

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTimesCircle, faExclamationCircle, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle, faExclamationCircle, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faExclamationCircle)
 library.add(faTimesCircle)
 library.add(faBars)
+library.add(faTimes)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
