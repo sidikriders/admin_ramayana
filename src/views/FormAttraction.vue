0b55831f7776e93c233a3f6bec4a78bc14ca789c
sidikriders <sidik@xwork.id>
2018-07-25 01:00:34 +0700

    create form for creating new attraction

--- src/views/FormAttraction.vue
+++ src/views/FormAttraction.vue
@@ -2,14 +2,93 @@
   <div class='router-view'>
     <!-- create new Type form  -->
     <div class='create-new'>
-      <h3>Create New Show Type</h3>
+      <h3 v-if="isCreate">Create New Attraction</h3>
+      <h3 v-else>Edit {{ attraction.name }}</h3>
+      <label class="checkbox">
+        <input type="checkbox" v-model="attraction.published">
+        Published
+      </label>
       <div class="input-group">
         <label>
-          New Type Name
+          Name
         </label>
-        <input type="text" v-model=newTypeName placeholder="New show type name" class="input">
+        <input type="text" v-model=attraction.name placeholder="Attraction's name" class="input">
+        <label>
+          English Name
+        </label>
+        <input type="text" v-model=attraction.nameEn placeholder="Attraction's english name" class="input">
       </div>
       <div class="input-group">
+        <label>
+          Attraction's Group
+        </label>
+        <div class="input-container">
+          <select id="attraction-group" v-model="attraction.attractionGroupId">
+            <option value="false">Attraction's Group</option>
+            <option v-for="(group, index) in groupList" :value="group.id" :key="index">{{ group.name }}</option>
+          </select>
+        </div>
+        <button class="rb-btn c" id="add-new-group" @click="addNew('group')">
+          Add new group
+        </button>
+      </div>
+      <div class="input-group">
+        <label>
+          Images
+        </label>
+        <div class="images-container">
+          <div class="image-unit" v-for="(img, index) in attractionImages" :key="index">
+            <font-awesome-icon icon="times" @click="deleteImage(index)"></font-awesome-icon>
+            <img
+            :src="img.data"
+            :alt="'attraction-image-' + index">
+          </div>
+        </div>
+        <input type="file" @change="addImage" accept='image/*'>
+      </div>
+      <div class="input-group">
+        <label>
+          Short Description
+        </label>
+        <div class="input-container">
+          <textarea type="text" v-model=attraction.shortDesc placeholder="Attraction's name" class="textarea" rows="3"></textarea>
+        </div>
+        <label>
+          English Short Description
+        </label>
+        <div class="input-container">
+          <textarea type="text" v-model=attraction.shortDescEn placeholder="Attraction's english name" class="textarea" rows="3"></textarea>
+        </div>
+      </div>
+      <div class="input-group">
+        <label>
+          Description
+        </label>
+        <div class="input-container">
+          <textarea type="text" v-model=attraction.desc placeholder="Attraction's name" class="textarea" rows="10"></textarea>
+        </div>
+        <label>
+          English Description
+        </label>
+        <div class="input-container">
+          <textarea type="text" v-model=attraction.descEn placeholder="Attraction's english name" class="textarea" rows="10"></textarea>
+        </div>
+      </div>
+      <div class="input-group">
+        <label>
+          Tags
+        </label>
+        <div class="input-container checkbox">
+          <label class="checkbox" v-for="(tag, index) in tagList" :key="index">
+            <input type="checkbox">
+            {{ tag.name }}
+          </label>
+        </div>
+        <button class="rb-btn c" id="add-new-tag" @click="addNew('tag')">
+          Add new tag
+        </button>
+      </div>
+      <!-- <div class="input-group">
         <label>
           Seating Map Image
         </label>
@@ -55,9 +134,9 @@
         @change="updateShowImg"
         accept='image/*'
         >
-      </div>
-      <button @click="addNewType" class="rb-btn p">
-        Add New Type
+      </div> -->
+      <button v-if="isCreate" @click="addNewAttraction" class="rb-btn p">
+        Add New Attraction
       </button>
     </div>
   </div>
@@ -68,36 +147,137 @@ export default {
   name: 'form-attraction',
   data: function () {
     return {
-      list: []
+      isCreate: true,
+      groupList: [],
+      tagList: [],
+      attraction: {
+        published: false,
+        name: '',
+        nameEn: '',
+        desc: '',
+        descEn: '',
+        shortDesc: '',
+        shortDescEn: '',
+        attractionGroupId: false
+      },
+      attractionImages: []
     }
   },
-  computed: {
-  },
   methods: {
-    deleteAttraction (at,tr) {
-      var isSure = confirm('Yakin mau hapus ' + attr.name + ' ya?')
-      if (isSure) {
-        alert('terhapus')
-      } else {
-        alert('cie galau')
+    fetchTags (cb) {
+      this.$store.getters._axios.get('/tags').then(resp => {
+        if (resp.data.status) {
+          this.tagList = resp.data.data
+          if (cb) {
+            cb()
+          }
+        } else {
+          this.$store.commit('showError', {
+            msg: 'Failed Fetch Tags List'
+          })
+        }
+      }).catch(err => {
+        console.error(err)
+        this.$store.commit('showError', {
+          msg: 'Failed Fetch Tags List'
+        })
+      })
+    },
+    fetchGroup (cb) {
+      this.$store.getters._axios.get('/attraction/group').then(resp => {
+        if (resp.data.status) {
+          this.groupList = resp.data.data
+          if (cb) {
+            cb()
+          }
+        } else {
+          this.$store.commit('showError', {
+            msg: 'Failed Fetch Group List'
+          })
+        }
+      })
+    },
+    addImage (event) {
+      var file = event.target.files[0]
+      var fileUrl = URL.createObjectURL(file)
+      this.attractionImages.push({
+        data: fileUrl,
+        file
+      })
+      event.target.value = ''
+    },
+    deleteImage (index) {
+      this.attractionImages.splice(index, 1)
+    },
+    addNew (_type) {
+      switch (_type) {
+        case 'tag':
+          var newTag = prompt('masukkan nama tag baru')
+          var newTagEn = prompt('now put new tag english name')
+          if (newTag && newTagEn) {
+            return this.$store.getters._axios.post('/tags', {
+              name: newTag,
+              nameEn: newTagEn
+            }).then(resp => {
+              if (resp.data.status) {
+                return this.fetchTags()
+              } else {
+                return this.$store.commit('showError', {
+                  msg: 'Gagal membuat tag baru'
+                })
+              }
+            }).catch(err => {
+              console.error(err)
+              return this.$store.commit('showError', {
+                msg: 'Gagal membuat tag baru'
+              })
+            })
+          } else {
+            break
+          }
+        case 'group':
+          var newGroup = prompt('masukkan nama group baru')
+          var newGroupEn = prompt('now put new group english name')
+          if (newGroup && newGroupEn) {
+            return this.$store.getters._axios.post('/attraction/group', {
+              name: newGroup,
+              nameEn: newGroupEn
+            }).then(resp => {
+              if (resp.data.status) {
+                return this.fetchGroup()
+              } else {
+                return this.$store.commit('showError', {
+                  msg: 'Gagal membuat tag baru'
+                })
+              }
+            }).catch(err => {
+              console.error(err)
+              return this.$store.commit('showError', {
+                msg: 'Gagal membuat tag baru'
+              })
+            })
+          } else {
+            break
+          }
+        default:
+          var _default = prompt('eaaa')
+          return _default
       }
     },
-    createNew () {
-      this.$router.push(this.$router.currentRoute.path + '/new')
+    addNewAttraction () {
+      this.attractionImages.forEach(el => {
+        console.log(el.file.name.slice(el.file.name.lastIndexOf('.')))
+      })
     }
   },
   beforeMount: function () {
-    document.title = 'Show Type'
-    this.$store.getters._axios.get('/attraction').then(resp => {
-      if (resp.data.status) {
-        this.list = resp.data.data
-      } else {
-        console.error(resp.data.data)
-        this.$store.dispatch('doLogout')
-      }
-    }).catch(err => {
-      console.error(err)
-    })
+    this.fetchGroup(this.fetchTags)
+    var id = this.$route.params.id
+    if (id === 'new') {
+      this.isCreate = true
+      document.title = 'Create New Attraction'
+    }
+    // this.$store.getters._axios.get('/attraction/')
   }
 }
 </script>
@@ -115,46 +295,58 @@ export default {
   display: flex;
   flex-direction: column;
   margin-bottom: 15px;
-  > input {
-    max-width: 530px;
-  };
-  img {
-    width: 300px;
-    height: 210px;
-    margin-bottom: 15px;
-    object-fit: contain;
-    background-color: #00000026;
-  };
   label {
     margin: 10px 0 5px;
   };
-  .price-type {
-    display: flex;
-    border-left: solid 4px grey;
-    margin-bottom: 15px;
-    .input-seat-detail {
-      display: flex;
-      flex-direction: column;
-      width: 150px;
-      margin: 10px 0 10px 20px;
-    }
-  }
-}
-
-.new-type-img-container {
-  display: flex;
-  flex-wrap: wrap;
-  .image-unit {
-    position: relative;
-    svg[data-icon="times"] {
-      position: absolute;
-      top: 10px;
-      left: 10px;
-      color: #8f8f8f;
-      font-size: 23px;
+  > input,
+  > .input-container {
+    max-width: 530px;
+    textarea {
+      resize: none;
     };
-    margin: 10px 20px 0 0;
-  }
+    select,
+    select {
+      width: 100%;
+      font-size: 14px;
+      padding: 5px;
+      option {
+        font-size: 17px;
+      }
+    };
+    label.checkbox {
+      margin: 0 20px 15px 0;
+    }
+  };
+  #add-new-group,
+  #add-new-tag {
+    font-weight: 400;
+    font-size: 12px;
+    padding: 6px 12px;
+    margin-top: 10px;
+  };
+  .images-container {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 15px 0 0;
+    .image-unit {
+      position: relative;
+      svg[data-icon="times"] {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        color: #8f8f8f;
+        font-size: 23px;
+      };
+      margin: 10px 20px 0 0;
+      img {
+        width: 300px;
+        height: 210px;
+        margin-bottom: 15px;
+        object-fit: contain;
+        background-color: #00000026;
+      };
+    }
+  };
 }
 
 svg[data-icon="times"] {
